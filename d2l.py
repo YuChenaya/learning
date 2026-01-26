46591afb71e731e578758aa454a5f3ed930a2474
yuchen <z2410706030@gmail.com>
2026-01-26 14:31:42 +0800

    Day10

--- d2l.py
+++ d2l.py
@@ -515,6 +515,7 @@ def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
           f'test acc {test_acc:.3f}')
     print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
           f'on {str(device)}')
+    plt.show()
 
 class Residual(nn.Module):
     def __init__(self, input_channels, num_channels,
